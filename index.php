f9ed32cb22b49f815136ce24a84489550ad18784
brightbitez <brightbite2009@gmail.com>
2019-02-12 16:30:15 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <input type="submit" value="Download CSV Form">
 </form>
 <form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
+    Select CSV file to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
