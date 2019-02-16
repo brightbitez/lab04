a18995859d950eea5ff73f0f98fd1f7d4baa0253
brightbitez <brightbite2009@gmail.com>
2019-02-16 23:45:51 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,13 +5,50 @@
 	</title>
 </head>
 <body>
+
+Name: <input type="text" name="name"><br>
+
+Exchange THB to JPY<br/>
+Instructions by step: <br/>
+1)Download the sample CSV File below 
 <form action="download.php">
   <input type="submit" value="Download CSV Form">
 </form>
+2)Rename your edited file to edited.csv then upload your edited CSV File below 
 <form action="upload.php" method="post" enctype="multipart/form-data">
     Select CSV file to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
+    <input type="submit" value="Upload" name="submit">
+    
+<?php
+function deleteExisted()
+{
+  $myFile = "uploads/edited.csv";
+  unlink($myFile);
+}
+if(array_key_exists('submit',$_POST)){
+   deleteExisted();
+}
+?>
 </form>
+3)Press Exchange Button down below <br>
+
+<form method="post">
+    <input type="submit" name ="exchange"value="Exchange" /><br/>
+</form>
+You will get JPY:
+<?php
+function calculateJpy(){
+  $f_pointer=fopen("uploads/edited.csv","r"); // file pointer
+  $ar=fgetcsv($f_pointer);
+  $amount = $ar[0]*$ar[1];
+  print $amount;
+}
+if(array_key_exists('exchange',$_POST)){
+  calculateJpy();
+}
+?>
+
+
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -1,31 +1,45 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body>
 <?php
-$target_dir = "";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        if (is_uploaded_file($_FILES['userfile']['tmp_name'])) {
-   echo "File ". $_FILES['userfile']['name'] ." uploaded successfully.\n";
-   echo "Displaying contents\n";
-   readfile($_FILES['userfile']['tmp_name']);
-} else {
-   echo "Possible file upload attack: ";
-   echo "filename '". $_FILES['userfile']['tmp_name'] . "'.";
-}
-
+            echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+            echo "Sorry, there was an error uploading your file.";
     }
+    
+
+//back button
+?>
+<form method="post">
+    <input type="submit" name ="back"value="Back" /><br/>
+</form>
+
+<?php
+
+function getback()
+{
+    header("Location: /index.php");
+    exit;
+}
+if(array_key_exists('back',$_POST)){
+   getback();
 }
 ?>
+
+
+</body>
+</html>
+
+
